d0368bd523dfc887514b0d0323761a971abf352e
CamiloMeassiMacoris <55770879+CamiloMeassiMacoris@users.noreply.github.com>
2024-06-07 19:46:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do curso.dev para levar o aprendizado de programação para o próximo nível!!!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Duda, eu amo você. Se você me ama, dá uma risadinha! 😻❤️</h1>
+  return <h1>Duda, eu amo você. Se você me ama, dá uma risadinha! 😻❤️</h1>;
 }
 
-export default Home; 
+export default Home;
